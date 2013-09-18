7efdb11456b3bf7fcd2f780ddeb3a577be20a9aa
Wesley Wigham <wes@ham.io>
2013-09-17 20:25:28 -0400

    Command line tool now has -h mode where it interprets input strings as hex, and outputs as hex strings

--- src/fishc.py
+++ src/fishc.py
@@ -3,10 +3,16 @@ from blowfish import Blowfish
 
 MODE_ENCRYPT = '-e'
 MODE_DECRYPT = '-d'
+MODE_READHEX = '-h'
 
-def main(mode, key, infile, outfile):
+def main(mode, key, infile, outfile, ashex):
+
     cipher = Blowfish(bytearray.fromhex(key))
     text = infile.read()
+    
+    if ashex:
+        text = bytearray.fromhex(text.strip())
+        print(text)
     
     for i in range(0,len(text),cipher.blockSize()):
         block = bytearray()
@@ -18,25 +24,44 @@ def main(mode, key, infile, outfile):
             for j in range(remainder):
                 block.append(0)
         
-        out = ()
+        out = b''
         
         if mode==MODE_ENCRYPT:
             out = cipher.encrypt(block)
         elif mode==MODE_DECRYPT:
             out = cipher.decrypt(block)
             
+        if ashex:
+            processed = ""
+            for num in out:
+                processed += hex(num)[2:]
+            out = processed
+            print(out)
+            
         outfile.write(out or b'')
  
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python fishc.py {-e/-d} key [inputfile] [outputfile]")
+        print("Usage: python fishc.py {-e/-d} ["+MODE_READHEX+"] key [inputfile] [outputfile]")
         sys.exit(1)
     else:
-        try:
-            infile,outfile = open(sys.argv[3], 'rb'),open(sys.argv[4], 'wb')
-            main(sys.argv[1], sys.argv[2], infile, outfile)
-        except:
-            infile = sys.stdin.buffer
-            outfile = sys.stdout.buffer
-            main(sys.argv[1], sys.argv[2], infile, outfile)
+        if sys.argv[2]==MODE_READHEX:
+            infile,outfile = None, None
+            try:
+                infile = open(sys.argv[4], 'r')
+                outfile = open(sys.argv[5], 'w')
+            except:
+                print("Couldn't open a file for reading and/or writing, using stdin and stdout instead", file=sys.stderr)
+                infile = sys.stdin
+                outfile = sys.stdout
+            main(sys.argv[1], sys.argv[3], infile, outfile, True)
+        else:
+            infile,outfile = None, None
+            try:
+                infile,outfile = open(sys.argv[3], 'rb'),open(sys.argv[4], 'wb')
+            except:
+                print("Couldn't open a file for reading and/or writing, using stdin and stdout instead", file=sys.stderr)
+                infile = sys.stdin.buffer
+                outfile = sys.stdout.buffer
+            main(sys.argv[1], sys.argv[2], infile, outfile, False)
