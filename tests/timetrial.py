8c6acb0548989033bbece874382c67e5c8aa6d7e
weswigham <wwigham@gmail.com>
2013-12-03 20:40:04 -0500

    how does this keep recurring

--- tests/timetrial.py
+++ tests/timetrial.py
@@ -11,18 +11,13 @@ class TestRunningTime(unittest.TestCase):
         self.n = 1
 
     def test_ntimes(self):
-        zero_key = bytes(b'\x00' * Blowfish.keySize())
+        zero_key = bytearray(b'\x00' * Blowfish.keySize())
         self.cipher = Blowfish(zero_key)
         for i in range(self.n):
             self.test_once()
-            if i % 1000 == 0:
-                if i % 100000 == 0:
-                    print()
-                print('.', end='')
-                sys.stdout.flush()
 
     def test_once(self):
-        zero_block = bytes(b'\x00' * Blowfish.blockSize())
+        zero_block = bytearray(b'\x00' * Blowfish.blockSize())
         self.cipher.encrypt(zero_block)
         #print(zero_block)
 
